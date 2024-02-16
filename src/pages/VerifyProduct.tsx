906cb6bd1bfa0acdb31477acf7d28b8dfc0f5b22
rakib8680 <rakib.cst.1st@gmail.com>
2024-02-16 19:30:57 +0600

    Add custom button and input field to VerifyProduct component

--- src/pages/VerifyProduct.tsx
+++ src/pages/VerifyProduct.tsx
@@ -1,10 +1,26 @@
+import { Input } from "@material-tailwind/react";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 
 const VerifyProduct = () => {
   return (
-     <div>
-         <h1>This is VerifyProduct component</h1>
-     </div>
-  )
+    <div className="container mx-auto py-10 h-[60vh] flex flex-col items-center justify-center gap-20">
+      <h1 className="text-center text-3xl font-black">Verify Your Product !</h1>
+      <div className="w-3/6 flex">
+        <Input
+          crossOrigin={""}
+          className="!text-xl !text-blue-gray-900"
+          size="lg"
+          variant="standard"
+          label="Unique ID"
+          placeholder="Enter Your Product's Unique ID"
+        />
+        <button className="custom-button flex items-center gap-2 px-3">
+          <p>Verify</p>
+          <FaArrowUpRightFromSquare size={14} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default VerifyProduct;
